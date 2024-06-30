880b880637771814765848c5531b36fbaeccd31b
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-06-30 08:46:42 -0300

    Understanding Event Binding

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       myName: "Vicente T",
       myAge: 32,
-      myLinkImage: 'https://images.unsplash.com/photo-1719306061122-23537e67f83e?q=80&w=2063&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      myLinkImage: 'https://picsum.photos/200/300'
     };
   },
   methods: {
